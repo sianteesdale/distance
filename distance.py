7743b13d855963cbe81f1bdd2e39d83b82fa8471
sianteesdale <43341988+sianteesdale@users.noreply.github.com>
2018-09-21 10:05:44 +0100

    Add computed dist

--- distance.py
+++ distance.py
@@ -2,3 +2,8 @@
 repository for computing the minimal distance between points
 """
 
+import math
+
+def compute_distance(point1, point2):
+    return math.sqrt((point1[0] - point2[0]) ** 2 + 
+                     (point1[1] - point2[1]) **2)
